f55fa5a39c42c672ee7127d96ac324a2656fc7ca
Anne-Sophie Desaulniers <2372588@edu.vaniercollege.qc.ca>
2023-11-25 11:32:04 -0500

    Strated writing the methods in the school management system class

--- src/main/java/org/anne_sophie/dto/SchoolManagementSystem.java
+++ src/main/java/org/anne_sophie/dto/SchoolManagementSystem.java
@@ -1,5 +1,45 @@
 package org.anne_sophie;
 
 public class SchoolManagementSystem {
+    public findDepartment(String Department) {
+        
+    }
+    public void printTeachers() {
+        
+    }
+    public void modifyCourseTeacher(String, String) {
+        
+    }
+    public void addDepartment(String) {
+        
+    }
+    public void printStudents() {
+        
+    }
+    public findStudents(String Student) {
+        
+    }
+    public void addCourse(String, double, String) {
+        
+    }
+    public void registerCourse(String, String) {
+        
+    }
+    public void addTeacher(String, String, String) {
+        
+    }
+    public findCourse(String Course) {
+        
+    }
+    public void printDepartment() {
+        
+    }
+    public void addStudent(String, String, String) {
+        
+    }
+    public findTeacher(String Teacher) {
+        
+    }
+    
 
 }
