27ae95f06262bbd8be12ffbbd1e317f1f9222196
Anne-Sophie Desaulniers <2372588@edu.vaniercollege.qc.ca>
2023-11-25 11:19:52 -0500

    Created the fields and methods for the department and course class

--- src/main/java/org/anne_sophie/dto/Course.java
+++ src/main/java/org/anne_sophie/dto/Course.java
@@ -1,5 +1,19 @@
 package org.anne_sophie.dto;
 
+import lombok.ToString;
+@ToString
 public class Course {
+    private double credit;
+    private String id;
+    private static int nextId = 1;
+    private Student[] students;
+    private Department department;
+    private int studentNum;
+    private Teacher teacher;
+    private String courseName;
+    
+    public Course(String courseName, double credit, Department department) {
+        
+    }
 
 }
--- src/main/java/org/anne_sophie/dto/Department.java
+++ src/main/java/org/anne_sophie/dto/Department.java
@@ -1,5 +1,14 @@
 package org.anne_sophie.dto;
 
+import lombok.ToString;
+@ToString
 public class Department {
+    private String id;
+    private static int nextId = 1;
+    private String departmentName;
+    
+    public Department(String departmentName) {
+        
+    }
 
 }
