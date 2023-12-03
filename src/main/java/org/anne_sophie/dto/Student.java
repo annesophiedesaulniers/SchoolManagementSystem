7d1a42fb642ba7d6e4d00106bb61f24580591105
Anne-Sophie Desaulniers <2372588@edu.vaniercollege.qc.ca>
2023-12-03 14:58:06 -0500

    Added a toString method

--- src/main/java/org/anne_sophie/dto/Student.java
+++ src/main/java/org/anne_sophie/dto/Student.java
@@ -2,6 +2,8 @@ package org.anne_sophie.dto;
 
 import lombok.ToString;
 
+import java.util.Arrays;
+
 /**
  * Student
  *
@@ -15,13 +17,20 @@ public class Student {
     private String studentId;
     private static int nextId = 1;
     private Department department;
+    private int courseNum;
 
     public Student(String fname, String lname, Department department) {
     this.fname = fname;
     this.lname = lname;
     this.department = department;
     this.studentId = String.format("S%03d", nextId++);
+    this.courses = new Course[5];
         }
+    public String toString() {
+        return "Student{id='" + studentId + "', fname='" + fname + "', lname='" + lname +
+                "', department=" + department + ", courseNum=" + courseNum +
+                ", courses=" + Arrays.toString(courses) + "}";
     }
+}
 
 
