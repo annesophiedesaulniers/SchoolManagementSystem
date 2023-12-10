358ad72cda5eb0496b8aa6f2411121c8bef7779f
Anne-Sophie Desaulniers <2372588@edu.vaniercollege.qc.ca>
2023-12-10 15:27:17 -0500

    made sure the methods work in the main

--- src/main/java/org/anne_sophie/Main.java
+++ src/main/java/org/anne_sophie/Main.java
@@ -11,15 +11,15 @@ public class Main {
         schoolManagementSystem.addTeacher("Yi", "Wang", "D001");
         schoolManagementSystem.addStudent("Anne-Sophie", "Desaulniers", "D001");
 
-        //schoolManagementSystem.displayTeachers();
-        //schoolManagementSystem.displayDepartments();
-        //schoolManagementSystem.displayCourses();
-        //schoolManagementSystem.displayStudents();
+        schoolManagementSystem.displayTeachers();
+        schoolManagementSystem.displayDepartments();
+        schoolManagementSystem.displayCourses();
+        schoolManagementSystem.displayStudents();
 
-        //System.out.println(schoolManagementSystem.findCourse("C001"));
-        //System.out.println(schoolManagementSystem.findTeacher("T001"));
-        //System.out.println(schoolManagementSystem.findStudent("S001"));
-        //System.out.println(schoolManagementSystem.findDepartment("D001"));
+        System.out.println(schoolManagementSystem.findCourse("C001"));
+        System.out.println(schoolManagementSystem.findTeacher("T001"));
+        System.out.println(schoolManagementSystem.findStudent("S001"));
+        System.out.println(schoolManagementSystem.findDepartment("D001"));
 
         schoolManagementSystem.modifyCourseTeacher("T001", "C001");
         schoolManagementSystem.registerCourse("C001", "S001");
